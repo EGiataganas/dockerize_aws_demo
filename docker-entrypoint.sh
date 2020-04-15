9abda413571273b3a41ccaf6d13b6f9d073e502d
Evangelos Giataganas <e.giataganas@gmail.com>
2020-04-16 00:29:28 +0100

    Fix to start the container when is connected with postgres

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+echo 'Waiting for a connection with postgres...'
+
+until psql -h "postgres" -U "postgres" -c '\q' > /dev/null 2>&1; do
+  sleep 1
+done
+
+echo "Connected to postgres..."
+
 echo "Bundling gems"
 bundle check || bundle install
 
